84264973c55b60d715d5b01e7f2dcac6d5487f93
KinLonChoi <kinlonchoi@gmail.com>
2021-11-15 09:33:50 +0000

    change in format
    Changed address from heading to a custom paragraph setting.
    Fixed some spacing issues that made the document look a bit too spaced out on top.

--- Auto CV Writer/cvmaker.py
+++ Auto CV Writer/cvmaker.py
@@ -4,6 +4,7 @@ from hyperlink import add_hyperlink
 from docx.oxml.ns import qn
 from os.path import abspath, dirname, join
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.enum.style import WD_STYLE_TYPE
 
 lines = []
 print("Paste job description here and type exit when finished:")
@@ -25,7 +26,9 @@ style.name = 'Arial'
 style.size = Pt(14)
 # Font change for heading and titles bugged in docx module asciiTheme also needed to be changed to  Arial.
 rFonts = style.element.rPr.rFonts
-rFonts.set(qn("w:asciiTheme"), "Arial")
+rFonts.set(qn('w:asciiTheme'), 'Arial')
+paragraph_format = document.styles['Heading 1'].paragraph_format
+paragraph_format.space_before = Pt(0)
 
 # Style for Heading 2
 style = document.styles['Heading 2'].font
@@ -33,49 +36,58 @@ style.color.rgb = docx.shared.RGBColor(0, 0, 0)
 style.name = 'Arial'
 style.size = Pt(13)
 rFonts = style.element.rPr.rFonts
-rFonts.set(qn("w:asciiTheme"), "Arial")
-
+rFonts.set(qn('w:asciiTheme'), 'Arial')
 
 # Style for Normal paragraph font
 style = document.styles['Normal'].font
 style.name = 'Arial'
 style.size = Pt(12)
 
+# Style used for information section under heading
+styles = document.styles
+style = styles.add_style('info', WD_STYLE_TYPE.PARAGRAPH)
+style.base_style = styles['Normal']
+style = document.styles['info'].font
+style.name = 'Arial'
+style.size = Pt(10)
+paragraph_format = document.styles['info'].paragraph_format
+paragraph_format.space_after = Pt(0)
 
-class Tools:
+class Format:
     def __init__(self, name, github):
         self.name = name
         self.github = github
         # Name and link to GitHub front page is centered
-        document.add_heading(name, level=1).alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-        p = document.add_paragraph("GitHub: ")
-        add_hyperlink(p, github, github)
-        p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        title = document.add_heading(name, level=1).alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        paragraph = document.add_paragraph("GitHub: ")
+        add_hyperlink(paragraph, github, github)
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        paragraph.style = document.styles['info']
 
-# The following three functions are made to improve readability of code
-# This is the font used for titles
+    # The following three functions are made to improve readability of code
+    # This is the font used for titles
     def title(self, header):
         document.add_heading(header, level=2)
 
-# This is the font used for paragraphs
+    # This is the font used for paragraphs
     def para(self, sentence):
         document.add_paragraph(sentence)
 
-# This is the font used for bullet points
+    # This is the font used for bullet points
     def bullet(self, sentence):
         document.add_paragraph(sentence, style='List Bullet')
 
-# Scans job description for keywords
+    # Scans job description for keywords
     def scan(self, words_lists):
         for items in words_lists:
             for k, v in items:
-                if k in ('front', 'html', 'css', 'web'):
+                if k in ('front', 'html', 'css', 'web', 'full'):
                     p = document.add_paragraph(v, style='List Bullet')
                     add_hyperlink(p,
                                   "GitHub Website.",
                                   "https://kinlonchoi.github.io/Portfolio/index.html")
                     break
-                elif k in ('python', 'js', 'javascript', 'back'):
+                elif k in ('python', 'js', 'javascript', 'back', 'full'):
                     add_hyperlink(document.add_paragraph(v, style='List Bullet'),
                                   "Python Projects",
                                   "https://github.com/KinLonChoi/Python-Projects")
@@ -87,12 +99,12 @@ class Tools:
                     continue
 
 
-cv = Tools("Kin Lon Choi", "https://github.com/KinLonChoi")
+cv = Format("Kin Lon Choi", "https://github.com/KinLonChoi")
+cv.para("")
 cv.para("I am a highly motivated individual with six years of experience in the science industry looking"
         " for a career change to pursue my interest in programming. "
         "I have self-taught knowledge of coding in both front and backend languages. "
         "I am keen to develop new skills required to become an integral part of a software team.")
-
 
 # Skills section add dictionary definition for each key words(search terms) with value(skills) to add as bullet point
 cv.title("Skills")
@@ -101,29 +113,29 @@ cv.title("Skills")
 # add_hyperlink(site, "https://kinlonchoi.github.io/Portfolio/index.html",
 #               "https://kinlonchoi.github.io/Portfolio/index.html")
 # Words to be searched for in job description
-web = dict.fromkeys(['front', 'html', 'css', 'web'],
+web = dict.fromkeys(['front', 'html', 'css', 'web', 'full'],
                     # if term is found print above as bullet point
                     "Well-versed in web technologies HTML and CSS with responsive designs using bootstrap framework. ")
 
-back = dict.fromkeys(['python', 'js', 'javascript', 'back'],
+back = dict.fromkeys(['python', 'js', 'javascript', 'back', 'full'],
                      "Working knowledge of backend languages JavaScript and Python. ")
-
-sql = dict.fromkeys(['sql', 'data', 'dbms'],
+# Some search terms are shortened to match variations of words e.g. analy will match analytical, analysis etc.
+sql = dict.fromkeys(['sql', 'data', 'dbms', 'analy'],
                     "SQL knowledge using Database Management Software SQLite.")
 
-algo = dict.fromkeys(['algo', 'data structure', 'sdlc', 'lifecycle'],
+algo = dict.fromkeys(['algo', 'data structure', 'sdlc', 'lifecycle', 'paradigm'],
                      "Familiar with programming paradigms such as algorithms, data structures and software development "
                      "lifecycles (SDLC).")
 
-time = dict.fromkeys(['pressure', 'result', 'time'],
+time = dict.fromkeys(['pressure', 'result', 'time', 'constraint'],
                      "Accustom to working under time pressure to deliver the results necessary to achieve customer"
                      " requirements in a timely fashion.")
 
-report = dict.fromkeys(['report', 'present'],
+report = dict.fromkeys(['report', 'present', 'analy'],
                        "Experience in data analytics, preparation, and presentation of technical reports according to"
                        " Good Manufacturing Practice (GMP) auditing standards.")
 
-team = dict.fromkeys(['team', 'independent'],
+team = dict.fromkeys(['team', 'independent', 'together'],
                      "Effective at working independently during product/method development and as a team to maintain"
                      " high-quality standards throughout the quality department.")
 
@@ -131,19 +143,18 @@ skill_list = [x.items() for x in (web, back, sql, algo, time, report, team)]
 
 cv.scan(skill_list)
 
-
 # Employment section same as before add dictionary definitions for search terms.
 cv.title("Employment")
 cv.para("Sept 2016 – Sept 2021               Tate & Lyle PLC	             Laboratory Analyst")
 
-special = dict.fromkeys(['standard', 'test'],
+special = dict.fromkeys(['standard', 'test', 'require', 'product'],
                         "Specialised in ensuring products meet the required standards through meticulous testing"
                         " throughout the production process.")
-# Some search terms are shortened to match variations of words e.g. strat will match strategy, strategic etc.
+
 design = dict.fromkeys(['strat' 'design', 'project'],
                        "Designed and implemented appropriate analytic strategies for unique projects.")
 
-collab = dict.fromkeys(['collab', 'team', 'improve'],
+collab = dict.fromkeys(['collab', 'team', 'improve', 'meet'],
                        "Collaborated with other departments and developed continuous improvement strategies.")
 
 audit = dict.fromkeys(['report', 'audit', 'feedback'],
@@ -156,7 +167,7 @@ cv.scan(employment_list_1)
 
 cv.para("Feb 2016 – Sept 2016                Tate & Lyle PLC                      Research scientist")
 
-plan = dict.fromkeys(['innovat', 'improve', 'projects'],
+plan = dict.fromkeys(['innovat', 'improve', 'projects', 'plan', 'continuous'],
                      "Planned and conducted experiments for the innovation of new product development and continuous"
                      " improvement projects.")
 
@@ -170,7 +181,6 @@ busy = dict.fromkeys(['prior', 'time', 'effici', 'busy'],
 employment_list_2 = [x.items() for x in (plan, meet, busy)]
 
 cv.scan(employment_list_2)
-
 
 # Education and Certification section
 cv.title("Education and Certification")
@@ -195,7 +205,6 @@ cv.para("2011-2015		MChem (Hons) in Chemistry (2:1) 							University of Leicest
 cv.para("2003-2011		The Bromfords School, Essex 								"
         "A-Levels in Chemistry (B), Biology (B), and Physics (B) 				"
         "10 GCSEs at A*-C including Chemistry, Maths, and English")
-
 
 # This will save file in same file directory as python file
 document.save(join(dirname(abspath(__file__)), "Kin Lon Choi.docx"))
