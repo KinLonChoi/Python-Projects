c1a1bf22e3fe92ce3b439c706ef201b5548df3d6
KinLonChoi <kinlonchoi@gmail.com>
2021-11-24 10:08:09 +0000

    Changed name and linked python project into words.
    Changed name to preferred name.
    Linked python GitHub to words to match website link in front end bullet point.

--- Auto CV Writer/cvmaker.py
+++ Auto CV Writer/cvmaker.py
@@ -19,7 +19,7 @@ job_desc = ('\n'.join(lines)).lower()
 excluded = docx.Document()
 excluded.save("Excluded.docx")
 document = docx.Document()
-document.save("Kin Lon Choi.docx")
+document.save("Michael Choi.docx")
 
 # Style for Heading 1
 style = document.styles['Heading 1'].font
@@ -67,6 +67,8 @@ class Format:
         self.github = github
         # Name and link to GitHub front page is centered
         document.add_heading(name, level=1).alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        document.add_paragraph("Wickford, Essex",
+                               style='info').alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
         email = document.add_paragraph("Email: ", style='info')
         add_hyperlink(email, "kinlonchoi@gmail.com", "kinlonchoi@gmail.com")
         document.add_paragraph("Mobile: 07712636191", style='info').alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
@@ -99,8 +101,9 @@ class Format:
                                   "https://kinlonchoi.github.io/Portfolio/index.html")
                     break
                 elif k in ('python', 'js', 'javascript', 'back', 'full'):
-                    add_hyperlink(document.add_paragraph(v, style='List Bullet'),
-                                  "Python Projects",
+                    p = document.add_paragraph(v, style='List Bullet')
+                    add_hyperlink(p,
+                                  "Python Projects.",
                                   "https://github.com/KinLonChoi/Python-Projects")
                     break
                 elif k in job_desc:
@@ -112,7 +115,7 @@ class Format:
                     continue
 
 
-cv = Format("Kin Lon Choi", "https://github.com/KinLonChoi")
+cv = Format("Michael Choi", "https://github.com/KinLonChoi")
 cv.para("")
 cv.para("I am a highly motivated individual with six years of experience in the science industry looking for a career "
         "change to pursue my passion in programming. In my spare time, I have taught myself knowledge of coding in both"
@@ -218,5 +221,5 @@ cv.para("2003-2011		The Bromfords School, Essex 								"
         "10 GCSEs at A*-C including Chemistry, Maths, and English")
 
 # This will save file in same file directory as python file
-document.save(join(dirname(abspath(__file__)), "Kin Lon Choi.docx"))
+document.save(join(dirname(abspath(__file__)), "Michael Choi.docx"))
 excluded.save(join(dirname(abspath(__file__)), "Excluded.docx"))
